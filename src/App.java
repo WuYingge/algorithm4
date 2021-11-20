82e0cf94d738f9b701f402f09d12f7fc1bcf4b68
wuyingge96@163.com <wuyingge96@163.com>
2021-11-21 01:15:19 +0800

    11-21

--- src/App.java
+++ src/App.java
@@ -2,6 +2,6 @@ import java.lang.System;
 
 public class App {
     public static void main(String[] args) {
-        System.out.print("Hello");
+        System.out.printf("%s", 1 + 2 + "3");
     }
 }
